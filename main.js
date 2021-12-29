2975d6863aeb9b2301a99b4cab2065d6ecad01d6
Francisco Burgos Santibañez <f.burgoss1589@gmail.com>
2021-12-29 01:31:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+//ingreso un numero para verificar si este es menor o mayor a 1000
+
+let numero = Number (prompt('ingrese un numero'));
+
+
+if(numero>=1000){
+    console.log('el numero ingresado es mayor a 1.000');
+    alert('el numero ingresado es mayor a 1.000');
+
+}
+else{
+    console.log('el numero ingresado es menor a 1.000');
+    alert('el numero ingresado es menor a 1.000');
+}
